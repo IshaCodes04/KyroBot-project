83c55061adb90b1a6412a20df977ccb38cdad81c
IshaCodes04 <ishu010.com@gmail.com>
2026-01-20 17:05:18 +0530

    yaay complete

--- Backend/src/routes/auth.routes.js
+++ Backend/src/routes/auth.routes.js
@@ -6,13 +6,11 @@ const passport = require('passport');
 router.post('/signup', signup);
 router.post('/login', login);
 
-const frontendURL = process.env.FRONTEND_URL || 'http://localhost:5173';
-
 // Google Auth Routes
 router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'], prompt: 'select_account' })); // Added prompt to force selector
 
 router.get('/google/callback',
-    passport.authenticate('google', { session: false, failureRedirect: `${frontendURL}/login` }),
+    passport.authenticate('google', { session: false, failureRedirect: '/login' }),
     googleCallback
 );
 
