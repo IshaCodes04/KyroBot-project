0d8161aba71248d95ddd9ee37ec43c49d8622c20
IshaCodes04 <ishu010.com@gmail.com>
2026-01-24 13:54:44 +0530

    add gemini key

--- Backend/src/service/ai.service.js
+++ Backend/src/service/ai.service.js
@@ -1,15 +1,21 @@
 const { GoogleGenAI } = require("@google/genai");
 
-const ai = new GoogleGenAI({});
+const ai = new GoogleGenAI({
+  apiKey: process.env.GEMINI_API_KEY,
+});
 
 async function generateResponse(chatHistory) {
-  const response = await ai.models.generateContent({
-    model: "gemini-2.0-flash",
-    contents: chatHistory ,
-  })
+  try {
+    const response = await ai.models.generateContent({
+      model: "gemini-2.0-flash",
+      contents: chatHistory,
+    });
 
-  return response.text
+    return response.text;
+  } catch (error) {
+    console.error("AI Generation Error:", error);
+    throw error;
+  }
 }
-
 
 module.exports = generateResponse;
